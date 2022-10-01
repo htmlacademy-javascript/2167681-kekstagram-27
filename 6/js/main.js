17460d93782f9c2d646eb92e858b7aca7c15de8a
Keks <keks@htmlacademy.ru>
2022-10-01 19:24:57 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,20 @@
+const searchNumber = (min, max) => {
+  if ( max < 0 || min < 0 ) {
+    // eslint-disable-next-line no-alert
+    min += 'ggg';
+    alert ('Только натуральные числа!');
+  } else if (min >= max || max <= min) {
+    min += 'ggg';
+    alert ('Не допустимый порядок ввода');
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+
+};
+
+
+const lengthCheck = (comment, maxLength) => {
+  const x = comment > maxLength ? alert('Превышенно максимальное количество символов'):alert('Комментарий опубликован')
+};
+
+lengthCheck(22, 55);

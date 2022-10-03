57fdc6867becd8133b9ded6b8eb1f08ede15407c
Shamee <nachozzzz224@gmail.com>
2022-10-03 18:13:10 +0300

    доработка на консультации

--- js/main.js
+++ js/main.js
@@ -1,26 +1,15 @@
-const searchNumber = (min, max) => {
-  let result;
-  if ( max < 0 || min < 0 ) {
-    result = 'NaN';
-  } else if (min > max || max < min) {
-    const swap = min;
-    min = max;
-    max = swap;
-    result = Math.floor(Math.random() * (max - min + 1)) + min;
-
-  } else {
-    result = Math.floor(Math.random() * (max - min + 1)	) + min;
+const getRandomNumber = (min, max) => {
+  if ( max < 0 || min < 0 ){
+    return NaN;
   }
-
-  return result;
-
-};
-searchNumber(111, 1111); /* Если ввести проверку searchNumber(111, fffff)
-                         в консоле появляется ошибка Uncaught Reference Error: fffff is not defined изменить на NaN
-								 так и не получилось. Если это критично, то думаю стоит провести консультацию.*/
-const lengthCheck = (comment, maxLength) => {
-  const x = comment.length > maxLength ? ('Превышенно максимальное количество символов') : ('Комментарий опубликован');
-  return x;
+  if ( min > max ) {
+    [min, max] = [max, min];
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-lengthCheck('ggggg', 5);
+getRandomNumber(10, 5);
+
+const lengthCheck = (comment, maxLength) => comment.length <= maxLength;
+
+lengthCheck('ggggg', 6);

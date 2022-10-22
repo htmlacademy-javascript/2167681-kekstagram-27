3316f437ecb3e82cc8924662b35e882682c1dd2a
Keks <keks@htmlacademy.ru>
2022-10-22 12:26:02 +0300

    :heavy_check_mark: Сборка #17

---
+++ 17/js/util.js
@@ -0,0 +1,75 @@
+import {avatarNumber} from './data.js';
+import {randomLikeRange} from './data.js';
+import {amountRandomComents} from './data.js';
+import {randomIPosition} from './data.js';
+import {DESCRIPTION} from './data.js';
+import {NAME} from './data.js';
+import {MESSAGE} from './data.js';
+
+// Генератор случайных чисел
+const getRandomNumber = (min, max) => {
+  if ( max < 0 || min < 0 ){
+    return NaN;
+  }
+  if ( min > max ) {
+    [min, max] = [max, min];
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+//Генератор массива случайной длинны
+const getRandomElement = (array) => {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+};
+
+//ограничение на длинну комментария
+const lengthCheck = (comment, maxLength) => comment.length <= maxLength;
+
+//Шаблон формы комментария
+const createComment = (i) => ({
+  id: i ,
+  avatar: `avatar-${ getRandomNumber(avatarNumber.min, avatarNumber.max)}.svg`,
+  message:getRandomElement(MESSAGE),
+  name: getRandomElement(NAME),
+});
+
+// Генератор случайного колличества комментариев
+const generateRandomArrayComments = () => {
+  const arrayComments = [];
+  for (let i = getRandomNumber(randomIPosition.min, randomIPosition.max); i <= getRandomNumber(amountRandomComents.min, amountRandomComents.max); i++) {
+    arrayComments.push(createComment(i));
+  }
+  return arrayComments;
+};
+
+// Шаблон формы профиля
+const createProfile = (i) => ({
+  id: i ,
+  url: `photos/${ i }.jpg`,
+  description: getRandomElement(DESCRIPTION),
+  likes: getRandomNumber (randomLikeRange.min, randomLikeRange.max),
+  comments: generateRandomArrayComments(),
+  avatar: `avatar-${ getRandomNumber(avatarNumber.min, avatarNumber.max)}.svg`,
+  message:getRandomElement(MESSAGE),
+  name: getRandomElement(NAME),
+
+});
+
+// Генератор массива профилей
+const generateArrayProfiles = () => {
+  const profiles = [];
+  for ( let i = 1; i <= 25 ; i++ ) {
+    profiles.push(createProfile(i));
+  }
+  return profiles;
+};
+
+
+export {
+  getRandomElement,
+  getRandomNumber,
+  lengthCheck,
+  generateArrayProfiles,
+};
+

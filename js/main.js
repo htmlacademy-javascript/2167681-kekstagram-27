7c904d013f4f04fe9aac8fca648d2ad0934168b2
Shamee <nachozzzz224@gmail.com>
2022-10-22 11:19:30 +0300

    отрисовка в крупном формате

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
 import {profilesList} from './printElement.js';
 
+
 // eslint-disable-next-line no-console
-console.log(profilesList);
+const thumbnails = document.querySelectorAll('.picture');
+const fullScreenPicture = document.querySelector('.big-picture');
+
+// теперь нужно сделать чтобы при клике открывалась такая же картинка//
+// чтобы открыть такое же изображение в полный экран нужно передать туда данные из миниатюры
+// переписывать в ручную каждый селектор??? // передать из элемента ссылку на фотографию
+
+const detailsWatching = (thumbnail) => {
+  thumbnail.addEventListener('click', () => {
+    fullScreenPicture.classList.remove('hidden');
+    let x = fullScreenPicture.querySelector('.big-picture__img');
+    thumbnails.forEach((elem) => {
+      x = elem.querySelector('.picture__likes');
+      console.log(x);
+    });
+  });
+};
+
+for (let i = 0; i < thumbnails.length; i++) {
+  detailsWatching(thumbnails[i]);
+}
+
+document.addEventListener('keydown', (evt)=> {
+  if (evt.keyCode === 27) {
+    fullScreenPicture.classList.add('hidden');
+  }
+});
+
+
 
--- js/printElement.js
+++ js/printElement.js
@@ -22,3 +22,4 @@ profilesList.appendChild(profileListFragment);
 export {
   profilesList,
 };
+
